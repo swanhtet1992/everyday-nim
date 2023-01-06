abb6ce8dfa3ee168cf021cbd6fdef72227d4ddc0
swanhtet1992 <swanhtet1992@gmail.com>
2023-01-06 22:04:23 +0630

    Day 5: exercise generated by chatgpt

---
+++ exe5.nim
@@ -0,0 +1,13 @@
+#[
+    Exercise: Write a function applyToAll that takes an sequence of numbers and a function
+    and returns a new sequence with the function applied to each element of the original array.
+]#
+import sequtils
+
+proc applyToAll(numbers: seq[int], fun: proc): seq[int] =
+    numbers.map do (x: int) -> int: fun(x)
+
+proc multiplyByTwo(x: int): int =
+    result = x * 2
+
+echo applyToAll(@[1, 2, 3], multiplyByTwo)
